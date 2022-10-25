666851e662160e150f29360fb1b0879ea6f33c3a
armancoding <arman.armament@gmail.com>
2022-10-25 19:09:54 -0400

    training ready

--- main.js
+++ main.js
@@ -1,37 +1,115 @@
-const canvas  = document.getElementById("myCanvas");
-canvas.width = 400;
+const carCanvas  = document.getElementById("carCanvas");
+carCanvas.width = 400;
 
-const ctx = canvas.getContext("2d");
-const road = new Road(canvas.width/2, canvas.width);
-const traffic =[
-    new Car(road.getLaneCenter(1),-100,60,100, "DUMMY", 5)  
-];
-const car = new Car(road.getLaneCenter(1),500, 60, 100, "KEYS");
+const networkCanvas  = document.getElementById("networkCanvas");
+networkCanvas.width = 600;
+
+const carCtx = carCanvas.getContext("2d");
+const networkCtx = networkCanvas.getContext("2d");
+
+const road = new Road(carCanvas.width/2, carCanvas.width);
+
+
+const N = 200;
+const ND = 5;
+
+const cars = generateCars(N);
+traffic = generateDummies(ND, -50);
+
+
+let bestCar = cars[0];
+
+if(localStorage.getItem("bestBrain"))
+{
+    for(let i = 0; i < cars.length; i++)
+    {
+        cars[i].brain = JSON.parse(localStorage.getItem("bestBrain"));
+        if(i != 0)
+        {
+            NeuralNetwork.mutate(cars[i].brain, 0.1);
+        }
+    }
+}
 
 
 animate();
 
-function animate()
+function save()
 {
+    localStorage.setItem("bestBrain", JSON.stringify(bestCar.brain));
+}
+
+function discard()
+{
+    localStorage.removeItem("bestBrain");
+}
+
+function generateCars(N)
+{
+    const cars = [];
+    for(let i =1; i <= N; i++)
+    {
+        cars.push(new Car(road.getLaneCenter(1), 100, 60, 100, "AI"));
+    }
+    return cars;
+}
+
+function generateDummies(N, best = 0)
+{
+    const traffic = [];
+    let y = best - 300;
+    for(let i =1; i <= N; i++)
+    {
+        y = y - 40 -100/(Math.random()+.2) ;
+        traffic.push(new Car(road.getLaneCenter(Math.round(Math.random()*3)), y, 60, 100, "DUMMY", 5));
+    }
+    return traffic;
+}
+
+function animate(time)
+{
+    bestCar = cars.find(c=>c.y == Math.min(...cars.map(c=>c.y)));
+    lastTraffic = traffic.find(c=>c.y == Math.min(...traffic.map(c=>c.y)));
+    if(lastTraffic.y > (bestCar.y+200))
+    {
+        delete traffic;
+        traffic = generateDummies(ND, bestCar.y);
+    }
+
     for(let i=0; i<traffic.length; i++)
     {
         traffic[i].update(road.borders, []);
     }
-    car.update(road.borders, traffic);
+
+    for(let i = 0; i < cars.length; i++)
+    {
+        cars[i].update(road.borders, traffic);
+    }
     
-    canvas.height = window.innerHeight;
+    carCanvas.height = window.innerHeight;
+    networkCanvas.height = window.innerHeight;
 
-    ctx.save();
-    ctx.translate(0, -car.y + canvas.height*.7);
+    carCtx.save();
+    carCtx.translate(0, -bestCar.y + carCanvas.height*.7);
 
-    road.draw(ctx);
+
+    road.draw(carCtx);
     for(let i=0; i<traffic.length; i++)
     {
-        traffic[i].draw(ctx, "blue");
+        traffic[i].draw(carCtx, "blue");
     }
-    car.draw(ctx, "yellow");
+    carCtx.globalAlpha = 0.2;
+    for(let i = 0; i < cars.length; i++)
+    {
+        cars[i].draw(carCtx, "yellow");
 
+    }
+    carCtx.globalAlpha = 1;
+    bestCar.draw(carCtx, "Orange", true)
 
-    ctx.restore();
+    carCtx.restore();
+
+    networkCtx.lineDashOffset = - time/50;
+    Visualizer.drawNetwork(networkCtx, bestCar.brain);
     requestAnimationFrame(animate);
 }
--- network.js
+++ network.js
@@ -1,3 +1,49 @@
+class NeuralNetwork
+{
+    constructor(neuronCounts)
+    {
+        this.levels=[];
+        for(let i = 0; i < neuronCounts.length-1; i++)
+        {
+            this.levels.push(new Level(neuronCounts[i], neuronCounts[i+1]));
+        }
+    }
+
+    static feedForward(givenInputs, network)
+    {
+        let outputs = Level.feedForward(givenInputs, network.levels[0]);
+        for(let i = 1; i < network.levels.length; i++)
+        {
+            outputs = Level.feedForward(outputs, network.levels[i]);
+        }
+        return outputs;
+    }
+
+    static mutate(network, amount=1)
+    {
+        network.levels.forEach(level => {
+            for(let i = 0; i < level.biases.length; i++)
+            {
+                level.biases[i] = lerp(level.biases[i], Math.random()* 2 - 1, amount)       
+            }
+            for(let i = 0; i < level.weights.length; i++)
+            {
+                for(let j = 0; j < level.weights[i].length; j++)
+                {
+                    level.weights[i][j] = lerp(
+                        level.weights[i][j],
+                        Math.random()*2-1,
+                        amount
+                    )
+                }
+            }
+        })
+    }
+}
+
+
+
+
 class Level
 {
     constructor(inputCount, outputCount)
@@ -6,10 +52,10 @@ class Level
         this.outputs = new Array(outputCount);
         this.biases = new Array(outputCount);
 
-        this.weigts = [];
+        this.weights = [];
         for(let i = 0; i < inputCount; i++)
         {
-            this.weigts[i] = new Array(outputCount);
+            this.weights[i] = new Array(outputCount);
         }
 
         Level.#randomize(this);
@@ -17,11 +63,11 @@ class Level
 
     static #randomize(level)
     {
-        for(i = 0; i < level.inputs.length; i++)
+        for(let i = 0; i < level.inputs.length; i++)
         {
-            for(j = 0; j < level.outputs.length; j++)
+            for(let j = 0; j < level.outputs.length; j++)
             {
-                level.weigts[i][j] = Math.random() * 2 - 1;//this way values are between -1 and 1;
+                level.weights[i][j] = Math.random() * 2 - 1;//this way values are between -1 and 1;
             }
         }
 
@@ -43,14 +89,15 @@ class Level
             let sum = 0;
             for(let j = 0; j < level.inputs.length; j++)
             {
-                if(sum > level.biases[i])
-                {
-                    level.outputs[i] = 1;
-                }
-                else 
-                {
-                    level.outputs[i] = 0;
-                }
+                sum += level.inputs[j] * level.weights[j][i];
+            }
+            if(sum > level.biases[i])
+            {
+                level.outputs[i] = 1;
+            }
+            else 
+            {
+                level.outputs[i] = 0;
             }
 
         }
